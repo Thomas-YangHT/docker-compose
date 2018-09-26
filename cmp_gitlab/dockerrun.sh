c389bf45dbdd0a13530eddabe750d65d61557e04
Thomas yang <168447636@qq.com>
2018-09-26 19:42:57 +0800

    1

---
+++ cmp_gitlab/dockerrun.sh
@@ -0,0 +1,10 @@
+docker run -d \
+  --hostname gitlab.yunwei.edu \
+  -p 1443:443 -p 1180:80 -p 122:22 \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.yunwei.edu/'; gitlab_rails['lfs_enabled'] = true;" \
+  --name gitlab \
+  --restart always \
+  -v /opt/cmp_gitlab/gitlab/config:/etc/gitlab:Z \
+  -v /opt/cmp_gitlab/gitlab/logs:/var/log/gitlab:Z \
+  -v /opt/cmp_gitlab/gitlab/data:/var/opt/gitlab:Z \
+192.168.254.211:5000/gitlab/gitlab-ce:latest
