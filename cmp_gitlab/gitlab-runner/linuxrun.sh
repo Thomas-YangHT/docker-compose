ea3ab286c591ccb849962ce4b9329757fc5ff24b
Thomas yang <168447636@qq.com>
2018-09-28 09:02:32 +0800

    1

---
+++ cmp_gitlab/gitlab-runner/linuxrun.sh
@@ -0,0 +1,7 @@
+wget -O /usr/local/bin/gitlab-runner https://raw.githubusercontent.com/Thomas-YangHT/docker-compose/master/cmp_gitlab/gitlab-runner-linux-amd64
+chmod +x /usr/local/bin/gitlab-runner
+# curl -sSL https://get.docker.com/| sh
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runnersudo gitlab-runner start
+usermod -aG docker gitlab-runner
+sudo -u gitlab-runner -H docker info
